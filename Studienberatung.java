71e1f89b258237f6091acc7a3f4a628f1012123c
Thomas <pommermann@hotmail.de>
2016-04-18 17:18:25 +0100

    New Version

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,11 +1,24 @@
 import java.util.*;
+
 public class Studienberatung{
+	
 	
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.println("Hast du Abitur?");
+		String antwort = scanner.next();
 		
-		String Antwort1 = scanner.nextln();
+		if(antwort.equals("ja")){
+			System.out.println("Bist du wissbegierdig?")
+			String antwort2 = scanner.next();
+		}
+		else if(antwort.equals("nein")){
+			System.out.println("Hast du Fachabitur?");
+			String antwort2 = scanner.next();
+			if(antwort2.equals("ja")){
+				System.out.println("Hast du eine Berufsausbildung?")
+				antwort3 = scanner.next();
+			}
+		}
 	}
 }
